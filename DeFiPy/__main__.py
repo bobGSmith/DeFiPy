6ac7f6bf30575d518fa56a444da907c63aa31e5c
Bob G Smith <bobbyatopk@gmail.com>
2023-05-31 23:57:11 +0100

    main2

--- DeFiPy/__main__.py
+++ DeFiPy/__main__.py
@@ -1,6 +1,7 @@
 import sys 
 
-print("DeFiPy")
+print("DeFiPy scripts")
 
-print("run as script under construction.. ")
+print("sign messages - `python3 DeFiPy.sign_message path_to_AES_encrypted_wallet_json message`" 
+ 
 
