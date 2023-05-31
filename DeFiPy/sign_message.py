ae6c4cc9ad4565f839a19a9a386427cf029f85d8
Bob G Smith <bobbyatopk@gmail.com>
2023-05-31 23:54:45 +0100

    sign message

--- DeFiPy/sign_message.py
+++ DeFiPy/sign_message.py
@@ -1,5 +1,11 @@
 '''DeFiPy - sign_message
 
+Module 
+======
+
+Can import sign_message function which can sign messages with a given private key.
+
+
 Script
 ======
 
@@ -13,7 +19,7 @@ from eth_account.messages import encode_defunct
 
 def sign_message(msg,private_key):
     msg = encode_defunct(msg)
-    return w3.eth.account.sign_message(message,private_key)
+    return w3.eth.account.sign_message(msg,private_key)
 
 if __name__ == '__main__':
     from DeFiPy import aes_load_wallets
@@ -27,5 +33,5 @@ if __name__ == '__main__':
     wallets = aes_load_wallets(sys.argv[1])
     account = input("select private key to use > ")
 
-    print(sign_message(sys.argv[2],wallets[account]))
+    print(sign_message(bytes(sys.argv[2],"utf-8"),wallets[account]))
 
