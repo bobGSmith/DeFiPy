9891d8102d5548a305a8983284ab88e037b03410
Bob G Smith <bobbyatopk@gmail.com>
2023-05-31 14:40:14 +0100

    init

--- DeFiPy/__init__.py
+++ DeFiPy/__init__.py
@@ -1,17 +1,17 @@
-from w3tools.aes_load_wallets import aes_load_wallets
-from w3tools.attachContracts import attachContracts, attach
-from w3tools.connect import connect, connect_with_local_account
-from w3tools.transact import transact
-from w3tools.getPrice import getUniV2Price, getSolidlyVarPrice, getPrice
-from w3tools.getAmountOut import getAmountOut, getAmountOutDecimal, getAmountOutSolidlyVol
-from w3tools.getReserves import getReservesSolidly, getReservesUni
-from w3tools.priceImpact import priceImpactSellSize, convergePrices, simulateArb, simulateTrade
-from w3tools.getBalance import getBalance
-from w3tools.approveSpend import approveSpend,multiApprove
-from w3tools.swapTokensForTokens import swapTokensForTokens
-from w3tools.cross_dex_arb import cross_dex_arb
-from w3tools.sort_pair import sort_pair
-from w3tools.decode_tx_input import decode_tx_input
-from w3tools.mempool import filter_pending
-from w3tools.sandwich import doubleTx
-from w3tools.deposit_weth import deposit_weth
+from .aes_load_wallets import aes_load_wallets
+from .attachContracts import attachContracts, attach
+from .connect import connect, connect_with_local_account
+from .transact import transact
+from .getPrice import getUniV2Price, getSolidlyVarPrice, getPrice
+from .getAmountOut import getAmountOut, getAmountOutDecimal, getAmountOutSolidlyVol
+from .getReserves import getReservesSolidly, getReservesUni
+from .priceImpact import priceImpactSellSize, convergePrices, simulateArb, simulateTrade
+from .getBalance import getBalance
+from .approveSpend import approveSpend,multiApprove
+from .swapTokensForTokens import swapTokensForTokens
+from .cross_dex_arb import cross_dex_arb
+from .sort_pair import sort_pair
+from .decode_tx_input import decode_tx_input
+from .mempool import filter_pending
+from .sandwich import doubleTx
+from .deposit_weth import deposit_weth
