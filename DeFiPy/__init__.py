c885d1294f81d7d4e9489a2d46a888be34b05e98
ovrhuman <71412394+ovrhuman@users.noreply.github.com>
2024-02-25 11:56:52 +0000

    init getPrice

--- DeFiPy/__init__.py
+++ DeFiPy/__init__.py
@@ -2,7 +2,7 @@ from .aes_load_wallets import aes_load_wallets
 from .attachContracts import attachContracts, attach, quick_attach
 from .connect import connect, connect_with_local_account
 from .transact import transact
-from .getPrice import getUniV2Price, getSolidlyVarPrice, getPrice
+from .getPrice import getUniV2Price, getSolidlyVarPrice, getPrice, getPriceCoinGecko
 from .getAmountOut import getAmountOut, getAmountOutDecimal, getAmountOutSolidlyVol
 from .getReserves import getReservesSolidly, getReservesUni
 from .priceImpact import priceImpactSellSize, convergePrices, simulateArb, simulateTrade
